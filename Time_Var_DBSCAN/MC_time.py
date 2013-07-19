522c7e0491ec7ea869226e2a927eda46e1381edd
carlson <erccarls@ucsc.edu>
2013-07-19 15:40:59 -0700

    First Commit

---
+++ Time_Var_DBSCAN/MC_time.py
@@ -0,0 +1,10 @@
+#===============================================================================
+# Generate an isotropic and homogenous event distribution to test sensitivity 
+# of dbscan to
+#===============================================================================
+
+
+import numpy as np
+import cPickle as pickle
+import matplotlib.pyplot as plt
+
