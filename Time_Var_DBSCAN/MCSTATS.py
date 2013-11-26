Fixed Major DBSCAN spherical indexing bug.  Results are now consistent.  Updated cluster size calculation for spherical coordinates (centroids not weighted)

Time_Var_DBSCAN/MCSTATS.py
@@ -61,7 +61,7 @@ def DBSCAN_Compute_Clusters(mcSims, eps, timeScale, min_samples ,nCorePoints = 3
     ################################################################
     # Compute cluster properties for each cluster in each simulation
     ################################################################
-    PROPS_PARTIAL = partial( __Cluster_Properties_Thread,BGDensity=BGDensity,TotalTime=TotalTime, inner=inner,outer=outer,sigMethod=sigMethod)
+    PROPS_PARTIAL = partial( __Cluster_Properties_Thread,BGDensity=BGDensity,TotalTime=TotalTime, inner=inner,outer=outer,sigMethod=sigMethod,metric=metric)
     if numProcs>1:
         p = pool.Pool(numProcs) # Allocate thread pool 
         ClusterResults = p.map(PROPS_PARTIAL,zip(dbscanResults,mcSims))
@@ -118,12 +118,14 @@ def __DBSCAN_THREAD(sim, eps, min_samples,timeScale,nCorePoints,plot=False,index
 
 def __Cluster_Properties_Thread(input,BGDensity,TotalTime, inner,outer,sigMethod,metric):
     labels,sim = input
-    
-    clusters   = np.array(np.int_(np.unique(labels)[1:])) # want to ignore the -1 for noise so ignore first element
-    CRLabels = clusters   
+    idx=np.where(labels!=-1)[0] # ignore the noise points
+    clusters = np.unique(labels[idx]).astype(int)
+#    clusters   = np.array(np.int_(np.unique(labels)[1:])) # want to ignore the -1 for noise so ignore first element
+    CRLabels = np.array(labels)
+
     # Some beurocracy because of way numpy array typecast handles things
     arrlen = len(np.unique(labels))
-    if arrlen == 1: 
+    if 0 not in labels: # no clusters 
         CR = ClusterResult.ClusterResult(Labels=[], Coords=[], 
                                      CentX=[]    , CentY=[]    , CentT=[], 
                                      Sig95X=[]  , Sig95Y=[]  , Sig95T=[], 
@@ -136,7 +138,7 @@ def __Cluster_Properties_Thread(input,BGDensity,TotalTime, inner,outer,sigMethod
     elif arrlen != 2: CRNumClusters = np.array(np.shape(clusters))[0] # Number of clusters found in the simulation
     elif arrlen == 2: 
         CRNumClusters = 1 # Number of clusters found in the simulation
-        CRLabels, clusters = [clusters,], [clusters,]
+        CRLabels, clusters = [np.array(labels),], [clusters,]
     
     CRCoords = [__Get_Cluster_Coords(sim, labels, cluster) for cluster in clusters] # contains coordinate triplets for each cluster in clusters.
 
@@ -154,12 +156,10 @@ def __Cluster_Properties_Thread(input,BGDensity,TotalTime, inner,outer,sigMethod
         CRSigsMethod = 'annulus'
         CRSigs   = np.array([DBSCAN.Compute_Cluster_Significance_3d_Annulus(CRCoords[cluster], np.transpose(sim), inner=inner, outer=outer) for cluster in range(len(clusters))])
     else: print 'Invalid significance evaluation method: ' , str(sigMethod)
-    # Compute sizes and centroids
-    CRSize95X, CRSize95Y, CRSize95T, CRPA, CRMedR, CRMedT, CRCentX,CRCentY,CRCentT,CRSig95X,CRSig95Y,CRSig95T = np.transpose([__Cluster_Size(CRCoords[cluster]) for cluster in range(len(clusters))])
     
     CRMembers = np.array([len(CRCoords[cluster]) for cluster in range(len(clusters))]) # count the number of points in each cluster.
     # Input into cluster results instance
-    CR = ClusterResult.ClusterResult(Labels=CRLabels, Coords=CRCoords, 
+    CR = ClusterResult.ClusterResult(Labels=np.array(CRLabels), Coords=CRCoords, 
                                      CentX=CRCentX    , CentY=CRCentY    , CentT=CRCentT, 
                                      Sig95X=CRSig95X  , Sig95Y=CRSig95Y  , Sig95T=CRSig95T, 
                                      Size95X=CRSize95X, Size95Y=CRSize95Y, Size95T=CRSize95T, 
@@ -216,20 +216,59 @@ def __Cluster_Size(cluster_coords):
 def __Cluster_Size_Spherical(cluster_coords):
     """Returns basic cluster properties, given a set of cluster coordinate triplets""" 
     X,Y,T = np.transpose(cluster_coords)
-    CentX0,CentY0,CentT0 = np.mean(X),np.mean(Y), np.mean(T)
-    #X, Y = X-CentX0, Y-CentY0
+    # Map to cartesian
+    X, Y = np.deg2rad(X), np.deg2rad(Y)
+    x = np.cos(X) * np.cos(Y)
+    y = np.cos(X) * np.sin(Y)
+    z = np.sin(X)
+
+    # Compute Cartesian Centroids
+    CentX0,CentY0,CentZ0,CentT0 = np.mean(x),np.mean(y), np.mean(z), np.mean(T)
+    r = np.sqrt(CentX0**2 + CentY0**2 + CentZ0**2)
     
-    #convert all distances to arc lengths using Vincenty's formula.
-    dPhi, dLam = X-CentX0,0
-    X = np.arctan(np.divide(np.sqrt( np.square( np.multiply(np.cos(X))),np.sin(dPhi) + np.square(np.cos(CentX0)*np.sin(X)-np.multiply(np.sin(CentX0)*np.cos(X),np.cos(dLam))))  , 
-                            (np.sin(CentX0)*np.sin(X)+np.cos(CentX0*np.multiply(np.cos(X), np.cos(dPhi)))) ))
-    dPhi, dLam = 0 , Y-CentY0
-    Y = np.arctan(np.divide(np.sqrt( np.square( np.multiply(np.cos(X))),np.sin(dPhi) + np.square(np.cos(CentX0)*np.sin(X)-np.multiply(np.sin(CentX0)*np.cos(X),np.cos(dLam))))  , 
-                            (np.sin(CentX0)*np.sin(X)+np.cos(CentX0*np.multiply(np.cos(X), np.cos(dPhi)))) ))
-    #Now that the distances are computed in terms of arc length and small, we treat them as euclidean.
+    # Rotate away Z components so we are in the x,z plane and properly oriented with galactic coordinates
+    def rotation_matrix(axis,theta):
+        axis = axis/np.sqrt(np.dot(axis,axis))
+        a = np.cos(theta/2.)
+        b,c,d = -axis*np.sin(theta/2.)
+        return np.array([[a*a+b*b-c*c-d*d, 2*(b*c-a*d), 2*(b*d+a*c)],
+                         [2*(b*c+a*d), a*a+c*c-b*b-d*d, 2*(c*d-a*b)],
+                         [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
+    # Pick axes
+    n = np.array([CentX0, CentY0,CentZ0])/r
+
+    n=np.array(n)/np.sqrt(np.dot(n,n))
+    nz = np.array([0.,0.,1.])
+    if (n!=nz).all():axis = np.cross(nz,n) 
+    else: axis = np.array([0,1,0])
+
+    theta = np.pi/2.-np.arccos(np.dot(nz,n))
+
+    R1 = rotation_matrix(axis,-theta)
+    print np.dot(R1,n)
+    theta2 = np.pi/2-np.arccos(np.dot(R1,n)[0])
+    R2 = rotation_matrix(nz,theta2)
+    R  = np.dot(R2,R1)
+    def rotate(n):
+        n = n/np.sqrt(np.dot(n,n))
+        return np.dot(R,n)
+
+    # rotate all the vectors (Y component should be zero for all)
+    X,Z,Y = np.rad2deg(np.transpose([rotate(np.transpose((x,y,z))[i]) for i in range(len(x))]))
     
+    #from matplotlib import pyplot as plt
+    #plt.figure(0)
+    #plt.scatter(X,Y)
+    #plt.axis('equal')
+    #plt.show()  
+    
+    # Convert Centroids back to lat/long in radians
+    CentY0 = np.rad2deg(np.arctan2(CentY0, CentX0))
+    CentX0 = np.rad2deg(np.arcsin(CentZ0/r))
+     
     # Singular Value Decomposition
     U,S,V = la.svd((X,Y))
+    
     # Rotate to align x-coord to principle component
     x = U[0][0]*X + U[0][1]*Y
     y = U[1][0]*X + U[1][1]*Y
@@ -237,6 +276,7 @@ def __Cluster_Size_Spherical(cluster_coords):
     # Compute weighted average and stdev in rotated frame
     weights = np.divide(1,np.sqrt(np.square(x)+np.square(y))) # weight by 1/r 
     CentX, CentY = np.average(x,weights=weights), np.average(y,weights=weights)
+    CentX,CentY =     CentX0, CentY0
     SigX,SigY = np.sqrt(np.average(np.square(x-CentX), weights=weights)), np.sqrt(np.average(np.square(y-CentY), weights=weights))
     
     # Find Position Angle
@@ -244,19 +284,20 @@ def __Cluster_Size_Spherical(cluster_coords):
     theta = -np.rad2deg(np.arctan2(xref,yref))
     POSANG = theta+90.
     
-    CentX ,CentY = np.dot(la.inv(U),(CentX,CentY)) #Translate the updated centroid into the original frame
-    CENTX,CENTY =  CentX+CentX0,CentY+CentY0          # Add this update to the old centroids
+   
+    #CentX ,CentY = np.dot(la.inv(U),(CentX,CentY)) #Translate the updated centroid into the original frame
+    #CENTX,CENTY =  CentX+CentX0,CentY+CentY0          # Add this update to the old centroids
     SIG95X,SIG95Y = 2*SigX/np.sqrt(np.shape(x)[0]),2*SigY/np.sqrt(np.shape(x)[0]) 
     SIZE95X, SIZE95Y = 2*S/np.sqrt(len(X)) 
-    
-    r = np.sqrt(np.square(X-CENTX)+np.square(y-CENTY))  # Build list of radii from cluster centroid
+    #r = np.sqrt(np.square(X-CENTX)+np.square(y-CENTY))  # Build list of radii from cluster centroid
+    r = np.sqrt(np.square(X-CentX0)+np.square(y-CentY0))  # Build list of radii from cluster centroid
     
     SIG95T = np.std(T)/np.sqrt(np.shape(r)[0])
     dt = np.abs(T-CentT0)
     countIndexT = int(np.ceil(0.95*np.shape(dt)[0]-1))
     SIZE95T = np.sort(dt)[countIndexT]   # choose the radius at this index
 
-    return SIZE95X, SIZE95Y,SIZE95T, POSANG, np.median(r), np.median(dt), CENTX,CENTY,CentT0,SIG95X,SIG95Y,SIG95T
+    return SIZE95X, SIZE95Y,SIZE95T, POSANG, np.median(r), np.median(dt), CentX0,CentY0,CentT0,SIG95X,SIG95Y,SIG95T
 
 
 

Time_Var_DBSCAN/dbscan3d_indexed.py
@@ -212,11 +212,6 @@ def dbscan3_indexed(X, eps, min_samples, timeScale, metric,indexing):
         #p.close()  # Kill pool after jobs complete.  required to free memory.
         #p.join()   # wait for jobs to finish.
 
-#        neighborhoods = [epsQuery(i) for i in range(0,n)] # serial version
-
-
-
-    #TODO: implement spherical refinement for real data
     elif (metric=='spherical'):
         #=========================================================================================
         # First query the grid
@@ -228,40 +223,41 @@ def dbscan3_indexed(X, eps, min_samples, timeScale, metric,indexing):
         if high_grid_lat >= GridSizeX: high_grid_lat=GridSizeX-1
         #def get_grid_points(k):  
         def __epsilonQuerySpherical(k):  
-            i,j = Xidx[k],Yidx[k]
-            il,ih = i-1, i+2 # select neighboring grid indices.
-            if XX[k]<85 and XX[k]>-85:
-                jl,jh = int(j-1./np.sin(np.abs(np.deg2rad(90-XX[k])))), int(j+1./np.abs(np.sin(np.deg2rad(90-XX[k])))+1) # np.sin(np.deg2rad(90-84))
-            # if within 10 degrees of either pole, return all points above or below
-            if il<=low_grid_lat:
-                jl,jh  = 0,GridSizeY # select all longitudes
-                il,ih  = 0,low_grid_lat+1
-            if ih>=high_grid_lat: 
-                jl,jh  = 0,GridSizeY-1 # select all longitudes
-                il,ih  = high_grid_lat-1, GridSizeX
-            idx = []
-            # if we span the line of 0 longitude, we need to break into 2 chunks.
-            if jl<0: 
-                idx = idx + [item for sublist in [item for sublist2 in Grid[il:ih,jl:] for item in sublist2] for item in sublist]
-                idx = np.array(idx + [item for sublist in [item for sublist2 in Grid[il:ih,0:jh] for item in sublist2] for item in sublist])
-            if jh >(GridSizeY-1):
-                idx = idx + [item for sublist in [item for sublist2 in Grid[il:ih,jl:] for item in sublist2] for item in sublist]
-                idx = np.array(idx + [item for sublist in [item for sublist2 in Grid[il:ih,0:jh-(GridSizeY)] for item in sublist2] for item in sublist])
-            else:
-                idx = np.array([item for sublist in [item for sublist2 in Grid[il:ih,jl:jh] for item in sublist2] for item in sublist])
+            if indexing == True:
+                i,j = Xidx[k],Yidx[k]
+                il,ih = i-1, i+2 # select neighboring grid indices.
+                if (XX[k]<85 and XX[k]>-85):
+                    jl,jh = int(j-1./np.sin(np.abs(np.deg2rad(90-XX[k])))), int(j+2./np.abs(np.sin(np.deg2rad(90-XX[k])))) # np.sin(np.deg2rad(90-84))
+                # if within 10 degrees of either pole, return all points above or below
+                if il<=low_grid_lat:
+                    jl,jh  = 0,GridSizeY # select all longitudes
+                    il,ih  = 0,low_grid_lat+1
+                if ih>=high_grid_lat: 
+                    jl,jh  = 0,GridSizeY # select all longitudes
+                    il,ih  = high_grid_lat-1, GridSizeX
+                idx = []
+                # if we span the line of 0 longitude, we need to break into 2 chunks.
+                if (jl<0 or jh > GridSizeY):
+                    
+                    idx = idx + [item for sublist in [item for sublist2 in Grid[il:ih,int(-2-2):] for item in sublist2] for item in sublist]
+                    idx = np.array(idx + [item for sublist in [item for sublist2 in Grid[il:ih,0:2+int(2.)] for item in sublist2] for item in sublist])
+                else:
+                    idx = np.array([item for sublist in [item for sublist2 in Grid[il:ih,jl:jh] for item in sublist2] for item in sublist])
+            
+            if indexing==False: idx = np.array(range(0,n)).astype(int) # select all points
             
             #Compute real arc lengths for these points.
             idx = np.append(idx,k).astype(int)
             j = -1 # index of original point in reduced list
             x = np.deg2rad(XX[idx])
             y = np.deg2rad(XY[idx])
-            dPhi = x-x[j]
-            dLam = y-y[j]
+            dPhi = x-x[j] # lat 
+            dLam = y-y[j] # lon
             # Distances using Vincenty's formula for arc length on a great circle.
-            d = arctan(np.divide(sqrt( square( np.multiply(cos(x),sin(dPhi))) + square(cos(x[j])*sin(x)-np.multiply(sin(x[j])*cos(x),cos(dLam))) ) , (sin(x[j])*sin(x)+cos(x[j])*np.multiply(cos(x), cos(dPhi)))))
+            d = np.arctan2(sqrt( square(cos(x)*sin(dLam) ) + square(cos(x[j])*sin(x)-sin(x[j])*cos(x)*cos(dLam)) ) , sin(x[j])*sin(x)+cos(x[j])*cos(x)*cos(dLam) )
             # Find where within time constraints
             tcut = np.logical_and(XT[idx] <= XT[k]+eps*float(timeScale),XT[idx] >= XT[k]-eps*float(timeScale))
-            rcut =d<np.deg2rad(eps)
+            rcut = d<np.deg2rad(eps)
             return idx[np.where(np.logical_and(rcut, tcut)==True)[0]] # This now contains indices of points in the eps neighborhood 
         #=========================================================================================
         neighborhoods = [ __epsilonQuerySpherical(k) for k in range(0,n)]
@@ -274,25 +270,6 @@ def dbscan3_indexed(X, eps, min_samples, timeScale, metric,indexing):
     core_samples = [] # A list of all core samples found.
     label_num = 0 # label_num is the label given to the new clust
 
-    if True==False:
-        labels = -np.ones(np.int(len(neighborhoods)))
-        samples = np.array([len(nhood) for nhood in neighborhoods]) # count in each eps neighborhoods
-        core_samples= np.where(samples>=min_samples)[0]             # Find where this is greater than threshold
-        label_num=0
-        # Now we want the core n_hoods in terms of core_sample indices.
-        SSThread = partial(__SSThread,core_samples=core_samples)
-        #p = pool.Pool(mp.cpu_count())
-        #core_nhoods = p.map(SSThread,neighborhoods) # this is a list of neighboring core points in core_samples indices.
-        core_nhoods = map(SSThread,neighborhoods) # this is a list of neighboring core points in core_samples indices.
-        #p.close()
-        #p.join()
-        
-        core_labels = -np.ones(np.int(len(core_samples))) # keeps track of cluster assignment
-        core_status = np.zeros(np.int(len(core_samples))) # keeps track of cluster expansion: 0/1/2 <-> unhandled/pending/added
-        core_labels == __ExpandClusters(core_labels,core_status,core_nhoods)
-        labels[core_samples]=core_labels 
-        return core_samples, labels
-    
     for index in range(0,n):
         if labels[index] != -1 or len(neighborhoods[index]) < min_samples:
             # This point is already classified, or not enough for a core point.
@@ -321,34 +298,9 @@ def dbscan3_indexed(X, eps, min_samples, timeScale, metric,indexing):
         # Current cluster finished.
         # Next core point found will start a new cluster.
         label_num += 1
-    #print "Core Samples", len(core_samples), " Distance Comps: ", ndist
     return core_samples, labels
 
-def __ExpandClusters(core_labels,core_status,core_nhoods):
-    chain = itertools.chain
-    label_num=0
-    while 0 in core_status:
-        idx = np.where(core_labels==-1)[0][0] # select first unassigned index 
-        core_labels[core_nhoods[idx]]=label_num # assign a cluster label
-        core_status[core_nhoods[idx]]=1         # flag as pending add
-        core_status[idx] = 2
-        core_labels[idx] = label_num
-        while 1 in core_status:   
-            idx = np.where(core_status==1)[0] # Find pending points
-            core_labels[idx]=label_num        # label the new points 
-            nhoods = np.fromiter(chain.from_iterable([core_nhoods[i] for i in idx]), dtype='int')
-            #for i in idx: core_status[nhoods[i][np.where(core_status[nhoods[i]]==0)[0]] ]=1
-            core_status[nhoods[np.where(core_status[nhoods]==0)[0]] ]=1
-            core_status[idx]=2                # flag them as added
-            
-        label_num+=1
-    return core_labels
-        
-  
 
-def __SSThread(nhood, core_samples):
-    intersection = np.intersect1d(core_samples,nhood,assume_unique=True)
-    return np.searchsorted(core_samples,intersection)
 
 def __epsQueryThread(k,Xidx,Yidx,GridSizeX,GridSizeY,Grid,XX,XY,XT,timeScale,eps,min_samples):
     """ Returns the epsilon neighborhood of a point for euclidean metric"""  
@@ -432,9 +384,6 @@ class DBSCAN(BaseEstimator, ClusterMixin):
         ----------
         X: array [n_samples X (or lat), n_samples Y (or long),n_samples T]
         """
-        if self.indexing== True:
-            self.core_sample_indices_, self.labels_ = dbscan3_indexed(X,**self.get_params())
-        else:
-            self.core_sample_indices_, self.labels_ = dbscan3(X,**self.get_params())
+        self.core_sample_indices_, self.labels_ = dbscan3_indexed(X,**self.get_params())
         return self
 